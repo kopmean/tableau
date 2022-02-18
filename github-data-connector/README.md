123f8393101f8561af31ed66c9b28ffdcf658a64
koppokung <73383077+kopmean@users.noreply.github.com>
2022-02-18 16:12:00 +0700

    Add files via upload

---
+++ github-data-connector/README.md
@@ -0,0 +1,47 @@
+<p align="left">
+<img src="tabird.png" alt="Tabird" width="50px" height="50px" align="left"/>
+<h1>Github Data Connector</h1>
+</p>
+
+A web data connector for Tableau to help you connect with GitHub data. 
+
+Quickly pull in GitHub information from different repositories such as:
+- Issues
+- Pull Requests
+- Traffic Stats
+
+An online version of the web data connector can be found here:
+- [https://github-web-data-connector.herokuapp.com/github/](https://github-web-data-connector.herokuapp.com/github/)
+
+An experimental version - including unreleased changes - can be found here:
+- [https://github-web-data-connector-dev.herokuapp.com/github/](https://github-web-data-connector-dev.herokuapp.com/github/)
+
+## Quick Tutorial
+1. Open Tableau, navigate to web data connector and enter the following url:
+https://github-web-data-connector.herokuapp.com/github/
+
+2. Authenticate with your GitHub account.
+![step2](https://cloud.githubusercontent.com/assets/8611594/24785643/7b6b56ea-1b10-11e7-9919-45427a66e510.png)
+
+3. Choose a data type and insert a valid url such as provided in the examples.
+![step3](https://cloud.githubusercontent.com/assets/8611594/24622827/3d24face-185b-11e7-825e-e432e2719c3f.png)
+
+4. Click get Data and choose your table and/or select the default connection to obtain all data.
+![step4](https://cloud.githubusercontent.com/assets/8611594/24622849/529f2032-185b-11e7-9355-e0f301fd7826.png)
+
+
+## Local development
+
+### Dependencies
+- NPM dependencies
+  - `npm install`
+
+### Build/Install
+- Run `gulp` from the command line.
+- Open Tableau and select Web Data Connector
+- Browse to http://localhost:9001
+- Fill in the required information and click 'Get Data'
+
+### Deployment
+- Set up a new OAuth application in [GitHub](https://github.com/settings/developers)
+- Copy the necessary variables to a local .env file.  
---
+++ github-data-connector/src/github/components/Traffic.js
@@ -0,0 +1,172 @@
+import GithubObject from './GithubObject';
+import $ from 'jquery';
+import _ from 'lodash';
+import debug from 'debug';
+
+const log = debug('traffic');
+
+/**
+ * Make simple API calls to the GitHub API.
+ */
+class Traffic extends GithubObject {
+
+  /**
+   * Initialize our GitHub API.
+   *
+   * @param {Github.auth} [auth]
+   *  The credentials used to authenticate with GitHub. If not provided
+   *  requests will be made unauthenticated.
+   * @param {string} [base]
+   *  The base of the API url.
+   */
+  constructor(auth, base) {
+    super(auth, base);
+  }
+
+  /**
+   * Returns the relevant GitHub schema objects for traffic stats.
+   *
+   * @return {Promise}
+   *  Promise of schema object.
+   */
+  getSchema() {
+    const schema = {
+      'tables': [],
+      'joins': [],
+    };
+
+    return new Promise((resolve, reject) => {
+      const tablePromises = [
+        Promise.resolve($.getJSON('/github/schema/repos.json')),
+        Promise.resolve($.getJSON('/github/schema/traffic_clones.json')),
+        Promise.resolve($.getJSON('/github/schema/traffic_popular_paths.json')),
+        Promise.resolve($.getJSON('/github/schema/traffic_popular_referrers.json')),
+        Promise.resolve($.getJSON('/github/schema/traffic_views.json'))],
+        joinPromises = Promise.resolve($.getJSON('/github/schema/_traffic_joins.json'));
+
+      Promise.all(tablePromises).then((tables) => {
+        schema.tables = tables;
+
+        return joinPromises;
+      }).then((joins) => {
+        schema.joins = joins;
+
+        resolve(schema);
+      });
+    });
+  }
+
+  /**
+   * Parse query into useful API request urls.
+   *
+   * @param {string} [url]
+   *  Request URI relative to the API base.
+   * @param {string} [tableId]
+   *  The table identifier.
+   * @return {Array}
+   *  Array of urls.
+   */
+  parseUrl(url, tableId) {
+    switch (tableId) {
+      case 'traffic_clones':
+        url += '/clones';
+        break;
+      case 'traffic_popular_paths':
+        url += '/popular/paths';
+        break;
+      case 'traffic_popular_referrers':
+        url += '/popular/referrers';
+        break;
+      case 'traffic_views':
+        url += '/views';
+        break;
+      case 'repos':
+        url = url.replace(/\/traffic/ig, '');
+        break;
+      default:
+        break;
+    }
+
+    return super.parseUrl(url, tableId);
+  }
+
+  /**
+   * Process our traffic stats into a format that is more Tableau friendly.
+   *
+   * @param {Object} [result]
+   *  An object where you wish to save the processed data onto.
+   * @param {Object} [table]
+   *  Table object which contains information about the columns and values.
+   * @param {Array} [rawData]
+   *  An array of objects to process.
+   * @returns {Promise}
+   */
+  processData(result, table, rawData) {
+    const tableId = table.tableInfo.id,
+      incrementalId = table.tableInfo.incrementColumnId,
+      incrementalValue = table.incrementValue;
+    let repo_owner, repo_name;
+    result[tableId] = [];
+
+    return new Promise((resolve, reject) => {
+      _.forEach(rawData, (obj) => {
+        // Parse out repo info.
+        if (_.has(obj, '_request_url')) {
+          const re = /repos\/([\w\-\.]+)\/([\w\-\.]+)/gi,
+            match = re.exec(obj['_request_url']);
+
+          if (match !== null) {
+            repo_owner = match[1];
+            repo_name = match[2];
+          }
+        }
+
+        switch (tableId) {
+          case 'traffic_views':
+            _.forEach(obj.views, (obj) => {
+              pushData(obj);
+            });
+            break;
+          case 'traffic_clones':
+            _.forEach(obj.clones, (obj) => {
+              pushData(obj);
+            });
+            break;
+          default:
+            pushData(obj);
+            break;
+        }
+      });
+
+      // Closure that handles pushing data onto the result stack.
+      function pushData(obj) {
+        obj.repo_name = repo_owner + '/' + repo_name;
+
+        // Add an additional timestamp field - if not provided by GitHub -
+        // to support incremental refreshes. We set the timestamp to the
+        // beginning of a day (i.e. 12 AM)
+        if (!_.has(obj, 'timestamp')) {
+          obj['timestamp'] = new Date(new Date().setHours(0,0,0,0)).toISOString();
+        }
+
+        // Handle incremental refresh.
+        if (incrementalId === 'timestamp' && incrementalValue) {
+          if (Date.parse(obj[incrementalId]) > Date.parse(incrementalValue)) {
+            result[tableId].push(obj);
+          }
+        }
+        else {
+          result[tableId].push(obj);
+        }
+      }
+
+      resolve(result);
+    });
+  }
+
+}
+
+export default Traffic;
+
+
+// Private helper functions.
